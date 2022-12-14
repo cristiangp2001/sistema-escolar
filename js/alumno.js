ac6428c06f2d05e55abd36d8291d2bcd804ac67c
cris <cris@MacBook-Pro-de-cris.local>
2022-12-13 18:32:35 -0600

    Creación de objeto

---
+++ js/alumno.js
@@ -0,0 +1,26 @@
+listaAlumno = [];
+
+function nuevoAlumno(anombre, dedad, calf1, calf2, calf3, calf4) {
+  var alumno = {
+    nombre: anombre,
+    edad: dedad,
+    cal1: calf1,
+    cal2: calf2,
+    cal3: calf3,
+    cal4: calf4,
+  };
+
+  listaAlumno.push(alumno);
+
+  let promedio = (calf1 + calf2 + calf3 + calf4) / 4;
+
+  alumnoListaHTML = `${alumno.nombre} ${alumno.edad} ${alumno.cal1} ${alumno.cal2} ${alumno.cal3} ${alumno.cal4} Tu promedio es de: ${promedio} `;
+  let parrafo = document.getElementById("elemento");
+  parrafo.classList.add("default-error");
+  parrafo.innerHTML = alumnoListaHTML;
+
+  listaAlumno.map(function(listaAlumno){
+    console.log(listaAlumno)
+  })
+  
+}
--- js/app.js
+++ js/app.js
@@ -1,46 +1,27 @@
+document.querySelector("#btnGuardar").addEventListener("click", guardarAlumno);
+document.querySelector("#btnRecorrer", recorrerAlumno);
+function guardarAlumno() {
+  event.preventDefault();
 
-function calcularPromedio() {
-    event.preventDefault();
+  dNombre = document.getElementById("nombre").value;
+  dEdad = document.getElementById("edad").value;
 
+  dmateria1 = parseFloat(document.getElementById("materia1").value);
+  dmateria2 = parseFloat(document.getElementById("materia2").value);
+  dmateria3 = parseFloat(document.getElementById("materia3").value);
+  dmateria4 = parseFloat(document.getElementById("materia4").value);
+  calificacion = document.getElementById('elemento');
 
-    dNombre = document.getElementById('nombre').value;
-    dEdad = document.getElementById('edad').value;
-
-    dmateria1 = parseFloat(document.getElementById('materia1').value);
-    dmateria2 = parseFloat(document.getElementById('materia2').value);
-    dmateria3 = parseFloat(document.getElementById('materia3').value);
-    dmateria4 = parseFloat(document.getElementById('materia4').value);
-
-
-    let prom = (dmateria1 + dmateria2 + dmateria3 + dmateria4) / 4;
-
-    calificacion = document.getElementById('elemento');
-
-
-     if(prom > 10.1 || prom != prom || prom < 0){
-      
-        elemento.classList.add('default-error');
-        elemento.innerHTML = "La calificación no puede ser mayor o menor a 10, o los campos están vacíos, verifica los campos nuevamente.";
-    }
-    
-    else if (prom >= 9 && prom <= 10) {
-        
-        elemento.classList.add('excelente');
-        elemento.innerHTML = "Los datos del alumno son " + dNombre + " tu edad es: "+  dEdad + "\n" + "\n\nTu promedio es de " + prom + " y es de excelencia";
-    }
-
-    else if (prom >= 8 && prom <= 8.9) {
-     
-        elemento.classList.add('aprobado'); 
-        elemento.innerHTML = "Los datos del alumno son " + dNombre + " tu edad es: "+ dEdad + "\n" + "\n\nTu promedio es de " + prom + " y es aprobatoria";
-    } else if (prom >= 7 && prom <= 7.9) {
-     
-        elemento.classList.add('satisfactorio');
-        elemento.innerHTML = "Los datos del alumno son " + dNombre + " tu edad es: "+ dEdad + "\n" + "\n\nTu promedio es de " + prom + " es satisfactorio";
-    } else {
-     
-        elemento.classList.add('reprobado');
-        elemento.innerHTML = "Los datos del alumno son " + dNombre + " tu edad es: "+ dEdad + "\n" + "\n\nTu promedio es de " + prom + " es reprobado";
-    }
-   
+ nuevoAlumno(dNombre, dEdad, dmateria1, dmateria2, dmateria3, dmateria4);
 }
+
+
+function recorrerAlumno(listaAlumno){
+  event.preventDefault();
+
+  for(i = 0; i<listaAlumno.length; i++){
+  console.log(nuevoAlumno[i]);
+  }
+}
+
+  
